09a4e35a1ea87b69722f919fcc4b72dc74072590
appels <appels@telenet.be>
2025-05-22 08:00:47 +0200

    maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:apple_maps_flutter/apple_maps_flutter.dart';
 
-void main() => runApp(const ScaffoldExampleApp());
+void main() => runApp(AppleMapsExample());
 
-class ScaffoldExampleApp extends StatelessWidget {
-  const ScaffoldExampleApp({super.key});
+class AppleMapsExample extends StatelessWidget {
+  late AppleMapController mapController;
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: ScaffoldExample());
+  void _onMapCreated(AppleMapController controller) {
+    mapController = controller;
   }
-}
-
-class ScaffoldExample extends StatefulWidget {
-  const ScaffoldExample({super.key});
-
-  @override
-  State<ScaffoldExample> createState() => _ScaffoldExampleState();
-}
-
-class _ScaffoldExampleState extends State<ScaffoldExample> {
-  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Sample Code')),
-      body: Center(child: Text('You have pressed the button $_count times.')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _count++),
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+            child: AppleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: const CameraPosition(
+                target: LatLng(0.0, 0.0),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }

acd4b354060adcdaaf97a134e2ee2f99336b5450
appels <appels@telenet.be>
2025-05-24 18:39:51 +0200

    Map test

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
-void main() => runApp(const MyApp());
-
+void main() => runApp(MyApp());  
+  
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
+  @override  
+  Widget build(BuildContext context) {  
+    return MaterialApp(  
+      title: 'OSM Flutter Application',  
+      theme: ThemeData(  
+        primarySwatch: Colors.blue,  
+      ),  
+      home: const OSMFlutterMap(),  
+    );  
+  }  
+}  
+
+class OSMFlutterMap extends StatefulWidget {
+  const OSMFlutterMap({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
+  State<OSMFlutterMap> createState() => _OSMFlutterMapState();
+}
 
-      ),
+class _OSMFlutterMapState extends State<OSMFlutterMap> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(),
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
     );
   }
 }
